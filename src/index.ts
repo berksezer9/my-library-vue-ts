cfbae8de6f45ae1fc4ee73f66af0f42b5be4eb9b
berksezer9 <berksezer9@gmail.com>
2024-04-21 14:54:10 +0300

    minor: exports session storage directly from src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 export * from './components';
-export * from './other/storage/session-storage'
+export { Retrieve } from "./other/storage/session-storage/Retrieve";
+export { Save } from "./other/storage/session-storage/Save";
+export { Transfer } from "./other/storage/session-storage/Transfer";
+export { StorageID } from "./other/storage/session-storage/StorageID";
+export * from "./other/storage/session-storage/storage-service";
+export * from "./other/storage/session-storage/TypesAndInterfaces";
